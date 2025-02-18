06aab5266b174bd6dd47f5f961257338f32e6bdf
100495801-UC3M <158502944+100495801-UC3M@users.noreply.github.com>
2025-02-18 14:38:35 +0100

    Add files via upload

---
+++ claves.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#ifndef claves_H
+#define claves_H
+
+struct Coord {
+    int x;
+    int y;
+    };
+
+int destroy(){
+
+}
+
+int set_value(int key, char *value1, int N_value2, double *V_value_2,
+    struct Coord value3){
+
+}
+
+int get_value(int key, char *value1, int *N_value2, double *V_value2,
+    struct Coord *value3){
+
+}
+
+int modify_value(int key, char *value1, int N_value2, double *V_value2,
+    struct Coord value3){
+
+}
+
+int delete_key(int key){
+
+}
+
+int exist(int key){
+
+}
+
+#endif
